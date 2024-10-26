8410e1c70bbee576cf6d2e2f797d7f66c897506d
mariprieto01 <noel.prieto@correo.ucu.edu.uy>
2024-10-26 20:05:22 -0300

    Arreglo error de cambio de contraseña

--- app.py
+++ app.py
@@ -14,10 +14,25 @@ def registro():
         return redirect(url_for('login'))
     return render_template('registro.html')
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET','POST'])
 def do_login():
     # Aquí puedes agregar la lógica para manejar el inicio de sesión
     return redirect(url_for('login'))
+
+@app.route('/cambiar_contrasena', methods=['GET', 'POST'])
+def cambiar_contrasena():
+    if request.method == 'POST':
+        usuario = request.form['usuario']
+        nueva_contrasena = request.form['nueva-contrasena']
+        confirmar_contrasena = request.form['confirmarContrasena']
+
+        if nueva_contrasena == confirmar_contrasena:
+            return redirect(url_for('login', mensaje='Contraseña cambiada exitosamente'))
+        else:
+            return redirect(url_for('cambiar_contrasena', mensaje='Las contraseñas no coinciden'))
+
+    mensaje = request.args.get('mensaje')
+    return render_template('cambiarContraseña.html', mensaje=mensaje)
 
 if __name__ == '__main__':
     app.run(debug=True)
