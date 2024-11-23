c6bb7ae79d0444ac948dab9d15c731d24646ab03
mariprieto01 <noel.prieto@correo.ucu.edu.uy>
2024-11-23 18:31:49 -0300

    Pull rama milagros2

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import mysql.connector
 
 app = Flask(__name__)
 
-# Función para obtener la conexión a la base de datos
 def get_db_connection():
     cnx = mysql.connector.connect(user='root', password='obligatorio', host='127.0.0.1', database='obligatorio')
     return cnx
@@ -14,7 +13,7 @@ def login():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')  # Asegúrate de tener este archivo HTML
+    return render_template('dashboard.html')
 
 @app.route('/alumnos')
 def alumnos_page():
@@ -81,7 +80,7 @@ def do_login():
         email = request.form['email']
         password = request.form['password']
 
-        print(f"Email recibido: {email}")  # Verifica el email recibido
+        print(f"Email recibido: {email}")
         print(f"Contraseña recibida: {password}")
 
         cnx = get_db_connection()
@@ -102,22 +101,18 @@ def do_login():
 
 @app.route('/buscar_instructores', methods=['POST'])
 def buscar_instructores():
-    # Obtener los valores del formulario
     nombre = request.form.get('nombreInstructor', '').strip()
     apellido = request.form.get('apellidoInstructor', '').strip()
     ci = request.form.get('ci', '').strip()
 
-    # Conectar a la base de datos
     cnx = get_db_connection()
     cursor = cnx.cursor(dictionary=True)
 
-    # Consulta base
     query = "SELECT ciInstructor, nombre, apellido"
     query += " FROM instructores"
     query += " WHERE 1=1"
     params = []
 
-    # Agregar condiciones dinámicas si los campos no están vacíos
     if ci:
         query += " AND ciInstructor LIKE %s"
         params.append('%' + ci + '%')
@@ -130,28 +125,23 @@ def buscar_instructores():
         query += " AND apellido LIKE %s"
         params.append('%' + apellido + '%')
 
-    # Ejecutar la consulta
     cursor.execute(query, params)
     instructores_result = cursor.fetchall()
     cursor.close()
     cnx.close()
 
-    # Pasar los resultados a la plantilla
     return render_template('instructores.html', instructores=instructores_result)
 
 @app.route('/buscar_alumnos', methods=['POST'])
 def buscar_alumnos():
-    # Obtener los valores del formulario
     nombre = request.form.get('nombre', '').strip()
     apellido = request.form.get('apellido', '').strip()
     descripcion = request.form.get('descripcion', '').strip()
     alquila = request.form.get('alquila', '').strip()
 
-    # Conectar a la base de datos
     cnx = get_db_connection()
     cursor = cnx.cursor(dictionary=True)
 
-    # Consulta base
     query = "SELECT ciAlumno, nombre, apellido, fecha_nacimiento, descripcion, alquila"
     query += " FROM alumnos"
     query += " INNER JOIN actividades ON actividades.idActividad = alumnos.idActividad"
@@ -174,34 +164,28 @@ def buscar_alumnos():
         query += " AND alquila LIKE %s"
         params.append('%' + alquila + '%')
 
-    # Ejecutar la consulta
     cursor.execute(query, params)
     alumnos_result = cursor.fetchall()
     cursor.close()
     cnx.close()
 
-    # Pasar los resultados a la plantilla
     return render_template('alumnos.html', alumnos=alumnos_result)
 
 @app.route('/buscar_equipamiento', methods=['POST'])
 def buscar_equipamiento():
-    # Obtener los valores del formulario
     id = request.form.get('id', '').strip()
     descripcion = request.form.get('descripcion', '').strip()
     actividad = request.form.get('actividad', '').strip()
     costo = request.form.get('costo', '').strip()
 
-    # Conectar a la base de datos
     cnx = get_db_connection()
     cursor = cnx.cursor(dictionary=True)
 
-    # Consulta base
     query = "SELECT idEquipamiento, descripcion, costo, idActividad"
     query += " FROM equipamiento"
     query += " WHERE 1=1"
     params = []
 
-    # Agregar condiciones dinámicas si los campos no están vacíos
     if id:
         query += " AND idEquipamiento LIKE %s"
         params.append('%' + id + '%')
@@ -218,28 +202,23 @@ def buscar_equipamiento():
         query += " AND costo LIKE %s"
         params.append('%' + costo + '%')
 
-    # Ejecutar la consulta
     cursor.execute(query, params)
     equipamiento_result = cursor.fetchall()
     cursor.close()
     cnx.close()
 
-    # Pasar los resultados a la plantilla
     return render_template('equipamiento.html', equipamiento=equipamiento_result)
 
 @app.route('/buscar_clases', methods=['POST'])
 def buscar_clases():
-    # Obtener los valores del formulario
     nombre = request.form.get('nombre', '').strip()
     actividad_clase = request.form.get('actividad_clase', '').strip()
     turno = request.form.get('turno', '').strip()
     dictada = request.form.get('dictada', '').strip()
 
-    # Conectar a la base de datos
     cnx = get_db_connection()
     cursor = cnx.cursor(dictionary=True)
 
-    # Consulta base
     query = "select idClase, idTurno, dictada, descripcion, nombre"
     query += " FROM clase"
     query += " inner join actividades a on clase.idActividad = a.idActividad"
@@ -248,7 +227,6 @@ def buscar_clases():
 
     params = []
 
-    # Agregar condiciones dinámicas si los campos no están vacíos
     if nombre:
         query += " AND nombre LIKE %s"
         params.append('%' + nombre + '%')
@@ -265,13 +243,11 @@ def buscar_clases():
         query += " AND dictada LIKE %s"
         params.append('%' + dictada + '%')
 
-    # Ejecutar la consulta
     cursor.execute(query, params)
     clases_result = cursor.fetchall()
     cursor.close()
     cnx.close()
 
-    # Pasar los resultados a la plantilla
     return render_template('clase.html', clases=clases_result)
 
 @app.route('/asignar_rol', methods=['GET', 'POST'])
@@ -279,12 +255,12 @@ def asignar_rol():
     if request.method == 'POST':
         nombre = request.form['txt']
         email = request.form['email']
-        rol = request.form['rol']  # Se obtiene el rol (en este caso solo puede ser 'administrador')
+        rol = request.form['rol']
 
         cnx = get_db_connection()
         cursor = cnx.cursor()
         print(f"Nombre: {nombre}, Email: {email}, Rol: {rol}")
-        # Query para actualizar el rol del usuario en la base de datos
+
         query = "UPDATE login"
         query += " SET rol = %s"
         query += " WHERE nombre = %s AND email = %s"
@@ -295,14 +271,9 @@ def asignar_rol():
         cursor.close()
         cnx.close()
 
-        # Redirigir a la página de asignar rol con un mensaje de éxito
         return render_template('asignar_rol.html', success="Rol asignado correctamente.")
 
-    # Si es una solicitud GET, solo se renderiza el formulario
     return render_template('asignar_rol.html')
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
